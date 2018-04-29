2f60b90ebbf3bc51d6e7862d6258d0e20c13f0a5
jjups96 <jjups96@gmail.com>
2018-04-28 22:58:55 -0700

    Modelo Aula

---
+++ database/migrations/2018_04_29_054012_create_aulas_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAulasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aulas', function (Blueprint $table) {
+            $table->increments('IdAula');
+			$table->string('Tipo', 25);
+			$table->integer('CantidadEquipo');
+			$table->integer('CantidadAV');
+			$table->integer('Capacidad');
+			$table->boolean('SillasPaleta');
+			$table->boolean('MesasTrabajo');
+			$table->boolean('Isotopica');
+			$table->boolean('Estrado');
+			$table->integer('Pizarron');
+			$table->integer('Illuminacion');
+			$table->integer('AislamientoR');
+			$table->integer('Ventilacion');
+			$table->integer('Temperatura');
+			$table->integer('Espacio');
+			$table->integer('Mobilario');
+			$table->integer('Conexiones');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aulas');
+    }
+}
