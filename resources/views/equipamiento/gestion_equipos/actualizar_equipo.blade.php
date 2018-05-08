300a08f3a74779efe47970fc2e9adc893679d545
francisco-tequida <francisco.tequida00@gmail.com>
2018-05-08 13:53:50 -0700

    puse mis formularios en tablas

--- resources/views/equipamiento/gestion_equipos/actualizar_equipo.blade.php
+++ resources/views/equipamiento/gestion_equipos/actualizar_equipo.blade.php
@@ -6,59 +6,88 @@
 	<a href="{{ url()->previous() }}">Regresar</a>
 	<form action="/gestion_equipo_modificar" method="POST">
 		<input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
-
 		<input type="hidden" name="id" value="{{$equipo->id}}">
 
-		<label for="serial">Serial: </label><br>
-		<input type="text" name="serial" value="{{$equipo->serial}}">
+		<table>
+			<tr>
+				<td>
+					<label for="serial">Serial: </label><br>
+				</td>
+				<td>
+					<input type="text" name="serial" value="{{$equipo->serial}}">
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="manual">Manual de usuario: </label>
+				</td>
+				<td>
+					@if($equipo->manualEquipo == 1)
+						<input type="radio" name="manual" value="1" checked>Sí <br>
+						<input type="radio" name="manual" value="0"> No
+					@else
+						<input type="radio" name="manual" value="1">Sí <br>
+						<input type="radio" name="manual" value="0" checked> No
+					@endif
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label>Operable: </label>
+				</td>
+				<td>
+					@if($equipo->operable == 1)
+						<input type="radio" name="operable" value="1" checked>Sí <br>
+						<input type="radio" name="operable" value="0"> No
+					@else
+						<input type="radio" name="operable" value="1">Sí <br>
+						<input type="radio" name="operable" value="0" checked> No
+					@endif
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="localizacion">Localización: </label>
+				</td>
+				<td>
+					<select name="localizacion">
+						@foreach($espacios as $espacio)
+							@if($equipo->localizacion == $espacio->clase)
+								<option value="{{$espacio->clase}}" selected>{{$espacio->clase}}</option>
+							@else
+								<option value="{{$espacio->clase}}">{{$espacio->clase}}</option>
+							@endif
+						@endforeach
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="software">Software: </label>
+				</td>
+				<td>
+					@foreach($software as $single_software)
+						<?php $temp = 0 ?>
+						
+						@foreach($equipo->software as $a)
+							@if($single_software->nombre == $a)
+								<?php $temp = 1 ?>
+							@endif
+						@endforeach
 
-		<br><br><label for="manual">Manual de usuario: </label><br>
-		@if($equipo->manualEquipo == 1)
-			<input type="radio" name="manual" value="1" checked>Sí <br>
-			<input type="radio" name="manual" value="0"> No <br><br>
-		@else
-			<input type="radio" name="manual" value="1">Sí <br>
-			<input type="radio" name="manual" value="0" checked> No <br><br>
-		@endif
-
-		<br><br><label>Operable: </label><br>
-		@if($equipo->operable == 1)
-			<input type="radio" name="operable" value="1" checked>Sí <br>
-			<input type="radio" name="operable" value="0"> No <br><br>
-		@else
-			<input type="radio" name="operable" value="1">Sí <br>
-			<input type="radio" name="operable" value="0" checked> No <br><br>
-		@endif
-
-		<br><br><label for="localizacion">Localización: </label><br>
-		<select name="localizacion">
-			@foreach($espacios as $espacio)
-				@if($equipo->localizacion == $espacio->clase)
-					<option value="{{$espacio->clase}}" selected>{{$espacio->clase}}</option>
-				@else
-					<option value="{{$espacio->clase}}">{{$espacio->clase}}</option>
-				@endif
-			@endforeach
-		</select>
-
-		<br><br><label for="software">Software: </label><br>
-		
-		@foreach($software as $single_software)
-			<?php $temp = 0 ?>
-			
-			@foreach($equipo->software as $a)
-				@if($single_software->nombre == $a)
-					<?php $temp = 1 ?>
-				@endif
-			@endforeach
-
-			@if($temp == 1)
-				<input type="checkbox" name="software[]" checked value="{{$single_software->nombre}}"> {{$single_software->nombre}}<br>
-			@else
-				<input type="checkbox" name="software[]" value="{{$single_software->nombre}}"> {{$single_software->nombre}}<br>
-			@endif
-		@endforeach
-
-		<input type = 'submit' value = "Modificar equipo"/>
+						@if($temp == 1)
+							<input type="checkbox" name="software[]" checked value="{{$single_software->nombre}}"> {{$single_software->nombre}}<br>
+						@else
+							<input type="checkbox" name="software[]" value="{{$single_software->nombre}}"> {{$single_software->nombre}}<br>
+						@endif
+					@endforeach
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<input type = 'submit' value = "Modificar equipo"/>	
+				</td>
+			</tr>
+		</table>
 	</form>
 @endsection
--- resources/views/equipamiento/gestion_equipos/agregar_equipo.blade.php
+++ resources/views/equipamiento/gestion_equipos/agregar_equipo.blade.php
@@ -7,31 +7,69 @@
 	<h1>Formulario para agregar un equipo</h1>
 	<form action="/gestion_equipo_agregar" method="POST">
 		<input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
+		<table>
+			<tr>
+				<td>
+					<label for="serial">Serial: </label>		
+				</td>
+				<td>
+					<input type="text" name="serial" placeholder="ID del equipo"> <br><br>
+				</td>
+			</tr>
 
-		<label for="serial">Serial: </label>
-		<input type="text" name="serial" placeholder="ID del equipo"> <br><br>
+			<tr>
+				<td>
+					<label for="manual">Se cuenta con manual: </label> <br>
+				</td>
+				<td>
+					<input type="radio" name="manual" value="1" checked="">Sí <br>
+					<input type="radio" name="manual" value="0"> No <br><br>				
+				</td>
+			</tr>
 
-		<label for="manual">Se cuenta con manual: </label> <br>
-		<input type="radio" name="manual" value="1" checked="">Sí <br>
-		<input type="radio" name="manual" value="0"> No <br><br>
+			<tr>
+				<td>
+					<label for="operable">Se encuentra operable: </label> <br>
+				</td>
+				<td>				
+					<input type="radio" name="operable" value="1" checked="">Sí <br>
+					<input type="radio" name="operable" value="0"> No <br><br>
+				</td>
+			</tr>
 
-		<label for="operable">Se encuentra operable: </label> <br>
-		<input type="radio" name="operable" value="1" checked="">Sí <br>
-		<input type="radio" name="operable" value="0"> No <br><br>
+			<tr>
+				<td>
+					<label for="localizacion">Localización: </label>
+				</td>
+				<td>
+					<select name="localizacion">
+						@foreach($espacios as $espacio)
+							<option value="{{$espacio->clase}}">{{$espacio->clase}}</option>
+						@endforeach
+					</select><br><br>
+				</td>
+			</tr>
 
-		<label for="localizacion">Localización: </label>
-		<select name="localizacion">
-			@foreach($espacios as $espacio)
-				<option value="{{$espacio->clase}}">{{$espacio->clase}}</option>
-			@endforeach
-		</select><br><br>
+			<tr>
+				<td>	
+					<label for="software">Software: </label><br>
+				</td>
+				<td>
+					@foreach($software as $single_software)
+						<input type="checkbox" name="software[]" value="{{$single_software->nombre}}"> {{$single_software->nombre}}<br>
+					@endforeach
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<input type = 'submit' value = "Agregar equipo"/>
+				</td>
+			</tr>
+		</table>
+		
 
-		<label for="software">Software: </label><br>
-		@foreach($software as $single_software)
-			<input type="checkbox" name="software[]" value="{{$single_software->nombre}}"> {{$single_software->nombre}}<br>
-		@endforeach
 
-		<input type = 'submit' value = "Agregar equipo"/>
+
 	
 	</form>
 @endsection
