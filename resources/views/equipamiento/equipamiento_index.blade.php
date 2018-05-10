a41f90e7c41f2196e62e141df22b7271712abe11
francisco-tequida <francisco.tequida00@gmail.com>
2018-05-09 23:50:14 -0700

    cambio de redireccion

--- resources/views/equipamiento/equipamiento_index.blade.php
+++ resources/views/equipamiento/equipamiento_index.blade.php
@@ -6,7 +6,7 @@
 	<a href="/">Regresar</a>
 	<h1>Equipamiento</h1>
 	<ul>
-		<li> <a href="equipamiento/software">Software</a> </li>
-		<li> <a href="/equipamiento/equipos">Equipos</a> </li>
+		<li> <a href="{{ url('/equipamiento/software') }}">Software</a> </li>
+		<li> <a href="{{ url('/equipamiento/equipos') }}">Equipos</a> </li>
 	</ul>
 @endsection
