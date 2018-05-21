9692b2c025c8636ad0121a6fb4f45fb89ec51892
francisco-tequida <francisco.tequida00@gmail.com>
2018-05-21 13:37:32 -0700

    emplo del inciso muy general

--- resources/views/incisos/seccion9_2/9_2_7.blade.php
+++ resources/views/incisos/seccion9_2/9_2_7.blade.php
@@ -1,11 +1,19 @@
 @extends('layouts.inciso')
 
-@section('contenido_formulario')
-	<div class="form-group">
-		<label for="serial" class="col-sm-4 control-label" data-toggle="tooltip" title="Ingrese el serial del equipo">Serial: </label>
+@section('formopen')
+    {{Form::open(['class'  => 'form-horizontal'])}}
+@endsection
 
-		<div class="col-sm-8">
-			<input type="text" class="form-control" id="serial" name="serial" placeholder="Serial del equipo" required>
+@section('contenido_formulario')
+
+	@foreach($preguntas as $pregunta)
+		<div class="form-group">
+			<label for="{{$pregunta->id}}" class="col-sm-4 control-label">{{$pregunta->titulo}}</label>
+
+			<div class="col-sm-8">
+				<input type="text" class="form-control" id="{{$pregunta->id}}" name="{{$pregunta->id}}" placeholder="Serial del equipo" required>
+			</div>
 		</div>
-	</div>
+	@endforeach
+
 @endsection
