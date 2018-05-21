9e15ded00a31e901eaab410be3073f27b1a19ecb
Jhon Smith <jsmith@localhost.localdomain>
2018-05-21 14:46:54 -0700

    Agrega campo espacio_id en vista create auditorio

--- resources/views/infraestructura/auditorios/create.blade.php
+++ resources/views/infraestructura/auditorios/create.blade.php
@@ -56,5 +56,13 @@
 			<input type='text' class="form-control" name='CantidadSanitarios' id="CantidadSanitarios" placeholder="1" required>
 		</div>
 	</div>
+
+	<div class="form-group">
+		<label for="espacio_id" class="col-sm-4 control-label" data-toggle="tooltip" title="Espacio donde se ecuentra la asesoría">Espacio</label>
+
+		<div class="col-sm-8">
+			<input type="radio"  name="espacio_id" value="{{$espacio_id}}" checked> {{$espacio_tipo}}
+		</div>
+	</div>
 @endsection
 
