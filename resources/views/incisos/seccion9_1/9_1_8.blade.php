d7d31d0e4c25401b3f424b1618ac2b3918d9ede8
RicardoHE97 <ricardo_110797@hotmail.com>
2018-05-22 10:01:19 -0700

    Vistas de incisos

---
+++ resources/views/incisos/seccion9_1/9_1_8.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.inciso')
+
+@section('formopen')
+    {{Form::open(['class'  => 'form-horizontal'])}}
+@endsection
+
+@section('contenido_formulario')
+
+	@foreach($preguntas as $pregunta)
+		<div class="form-group">
+			<label for="{{$pregunta->id}}" class="col-sm-4 control-label">{{$pregunta->titulo}}</label>
+
+			<div class="col-sm-8">
+				<input type="text" class="form-control" id="{{$pregunta->id}}" name="{{$pregunta->id}}">
+			</div>
+		</div>
+	@endforeach
+
+@endsection
