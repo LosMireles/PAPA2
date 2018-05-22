0626747e3fbffd811bc466a6224ff978c0a25041
Gilberto Espinoza <gilbertoespinoza97@gmail.com>
2018-05-22 16:04:35 -0700

    Seed de cursos

--- database/seeds/CursoTableSeeder.php
+++ database/seeds/CursoTableSeeder.php
@@ -11,8 +11,58 @@ class CursoTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Curso::class, 2)->create()->each(function ($u) {
-            $u->save();
-        });
+        App\Curso::create(array(
+            'nombre'        => 'Calculo Diferencial e Integral 2',
+            'periodo'       => '2018-1',
+            'grupo'         => '33',
+            'noEstudiantes' => '30',
+            'tipoAula'      => 'Aula',   # Laboratorio # Otro
+            'pertenencia'   => 'LCC',   # LM # Otro
+        ));
+
+        App\Curso::create(array(
+            'nombre'        => 'Mecanica con Laboratorio',
+            'periodo'       => '2018-1',
+            'grupo'         => '6',
+            'noEstudiantes' => '18',
+            'tipoAula'      => 'Laboratorio',   # Laboratorio # Otro
+            'pertenencia'   => 'LCC',   # LM # Otro
+        ));
+
+        App\Curso::create(array(
+            'nombre'        => 'Algebra Lineal',
+            'periodo'       => '2018-1',
+            'grupo'         => '4',
+            'noEstudiantes' => '20',
+            'tipoAula'      => 'Aula',   # Laboratorio # Otro
+            'pertenencia'   => 'LCC',   # LM # Otro
+        ));
+
+        App\Curso::create(array(
+            'nombre'        => 'Introduccion a Ciencias de la Computación',
+            'periodo'       => '2018-1',
+            'grupo'         => '1',
+            'noEstudiantes' => '40',
+            'tipoAula'      => 'Aula',   # Laboratorio # Otro
+            'pertenencia'   => 'LCC',   # LM # Otro
+        ));
+
+        App\Curso::create(array(
+            'nombre'        => 'Diseño de Algoritmos',
+            'periodo'       => '2018-1',
+            'grupo'         => '1',
+            'noEstudiantes' => '40',
+            'tipoAula'      => 'Aula',   # Laboratorio # Otro
+            'pertenencia'   => 'LCC',   # LM # Otro
+        ));
+
+        App\Curso::create(array(
+            'nombre'        => 'Matematicas Discretas',
+            'periodo'       => '2018-1',
+            'grupo'         => '1',
+            'noEstudiantes' => '40',
+            'tipoAula'      => 'Aula',   # Laboratorio # Otro
+            'pertenencia'   => 'LCC',   # LM # Otro
+        ));
     }
 }
