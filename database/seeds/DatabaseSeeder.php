da99c36b888a0b00ea29d6934571c0c33d5eac08
JhonSmith <jhonsmith@localhost.localdomain>
2018-05-22 23:59:35 -0700

    Arregla llaves foraneas en seeder
    En DatabaseSeeder las relaciones que sean de muchos a muchos ahora
    deberian funcionar correctamente y se meten 3 al azar de uno en otro.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -25,14 +25,37 @@ class DatabaseSeeder extends Seeder
         $this->call(AulaTableSeeder::class);
 
 
-        $equipos   = App\Equipo::all();
+        $equipos     = App\Equipo::with('softwares')->get();
+        $softwaresId = App\Software::pluck('id');
         foreach($equipos as $equipo){
-            $equipo->softwares()->attach(random_int(1, 10));
+            if(sizeof($softwaresId) > 3){
+                $equipo->softwares()->attach($softwaresId->random(3));
+            }
+            else{
+                $equipo->softwares()->attach($softwaresId);
+            }
         }
 
-        $espacios = App\Espacio::all();
+        $espacios = App\Espacio::with('cursos')->get();
+        $cursosId = App\Curso::pluck('id');
         foreach($espacios as $espacio){
-            $espacio->cursos()->attach(random_int(15,20));
+            if(sizeof($cursosId) > 3){
+                $espacio->cursos()->attach($cursosId->random(3));
+            }
+            else{
+                $espacio->cursos()->attach($cursosId);
+            }
         }
+
+        $asignaturas = App\Asignatura::with('softwares')->get();
+        foreach($asignaturas as $asignatura){
+            if(sizeof($softwaresId) > 3){
+                $asignatura->softwares()->attach($softwaresId->random(3));
+            }else{
+                $asignatura->softwares()->attach($softwaresId);
+            }
+        }
+
     }
 }
+
