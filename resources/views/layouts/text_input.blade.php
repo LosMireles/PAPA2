5758b8a94f46f3271711a6336bc2c5d53403f7da
JhonSmith <jhonsmith@localhost.localdomain>
2018-05-24 22:20:06 -0700

    Generaliza mas plantilla text_input
    Ahora se puede especificar el tipo de input (integer, date, time, etc) y
    por default es text-center.

--- resources/views/layouts/text_input.blade.php
+++ resources/views/layouts/text_input.blade.php
@@ -1,12 +1,12 @@
-	<div class="form-group">
+<div class="form-group">
 
-		<label for="{{$nombre_input}}" class="col-sm-4 control-label" data-toggle="tooltip" title="{{$tooltip_input}}">
-		    {{$label_input}}
-		</label>
+    <label for="{{$nombre_input}}" class="col-sm-4 control-label" data-toggle="tooltip" title="{{$tooltip_input}}">
+        {{$label_input}}
+    </label>
 
-		<div class="col-sm-8">
-      		<input type="text-center" class="form-control" name="{{$nombre_input}}" placeholder="{{$placeholder_input or ""}}" value="{{$valor_default or ""}}" {{$extra}})>
-      	</div>
+    <div class="col-sm-8">
+        <input type="{{$tipo or 'text-center'}}" class="form-control" name="{{$nombre_input}}" placeholder="{{$placeholder_input or ""}}" value="{{$valor_default or ""}}" {{$extra}})>
+    </div>
 
-	</div>
+</div>
 
