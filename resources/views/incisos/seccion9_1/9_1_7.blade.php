4ec9157a7217a1d62c272ba3780541038c441bb2
francisco-tequida <francisco.tequida00@gmail.com>
2018-05-27 14:52:53 -0700

    inciso jordan 9.1.7

--- resources/views/incisos/seccion9_1/9_1_7.blade.php
+++ resources/views/incisos/seccion9_1/9_1_7.blade.php
@@ -24,56 +24,12 @@
 			</label>
 
 			<div>
-                <textarea class="form-control" id="{{$pregunta->id}}" name="{{$pregunta->id}}"value="{{$pregunta->respuesta}}"></textarea>
+                <textarea class="form-control" id="{{$pregunta->id}}" name="{{$pregunta->id}}">{{$pregunta->respuesta}}</textarea>
 			</div>
 		</div>
 	@endforeach
-
-	<div class="row text-right" style="margin: 2px;">
-    	<a href="{{ action('CursoController@create') }}" class="btn btn-success">Agregar</a>
-  	</div>
-	@component('layouts.componentes.tabla_incisos_agregar')
-
-	<!-- Tabla de todos los grupos -->
-	@slot('cabeza_tabla')
-		<th>Nombre</th>
-		<th>Período</th>
-		<th>Grupo</th>
-		<th>Numero de Estudiantes</th>
-		<th>Pertenencia</th>
-		<th>Espacios</th>
-	@endslot
-
-	@slot('cuerpo_tabla')
-	  @foreach ($cursos as $curso)
-		<tr>
-		  	<td>{{ $curso->nombre }}</td>
-		  	<td>{{ $curso->periodo }}</td>
-		  	<td>{{ $curso->grupo }}</td>
-		  	<td>{{ $curso->no_estudiantes }}</td>
-		  	<td>{{ $curso->departamento }}</td>
-			@if(!empty($curso->aulas))
-				@foreach($curso->aulas as $aula)
-					<td>{{$aula->nombre}}</td> <!--Solo para uno, cambiar foreach en caso de mas-->
-				@endforeach
-			@endif
-
-		    <td>
-              <a href="{{ action('CursoController@edit', $curso->nombre) }}" class="btn btn-warning">Editar</a>
-            </td>
-
-			@component('layouts.boton_borrar')
-              @slot('controlador_borrar')
-                {{ Form::open(['action' => ['CursoController@destroy', $curso->nombre]]) }}
-              @endslot
-            @endcomponent
-
-
-		</tr>
-	  @endforeach
-	@endslot
-  @endcomponent
 @endsection
+
 @section('boton_guardar')
     <div class="text-center">
       <div>
@@ -90,4 +46,53 @@
         {{ Form::submit('Guardar', ['class' => 'btn btn-success']) }}
         {{ Form::close() }}
     </div>
+@endsection
+
+@section('evidencias_tabla')
+	<div class="row text-right" style="margin: 2px;">
+    	<a href="{{ action('CursoController@create') }}" class="btn btn-success">Agregar</a>
+  	</div>
+	@component('layouts.componentes.tabla_incisos_agregar')
+
+		<!-- Tabla de todos los grupos -->
+		@slot('cabeza_tabla')
+			<th>Nombre</th>
+			<th>Período</th>
+			<th>Grupo</th>
+			<th>Numero de Estudiantes</th>
+			<th>Pertenencia</th>
+			<th>Espacios</th>
+		@endslot
+
+		@slot('cuerpo_tabla')
+		  @foreach ($cursos as $curso)
+			<tr>
+			  	<td>{{ $curso->nombre }}</td>
+			  	<td>{{ $curso->periodo }}</td>
+			  	<td>{{ $curso->no_grupo }}</td>
+			  	<td>{{ $curso->no_estudiantes }}</td>
+			  	<td>{{ $curso->departamento }}</td>
+				@if(!empty($curso->aulas))
+					@foreach($curso->aulas as $aula)
+						<td>{{$aula->nombre}}</td> <!--Solo para uno, cambiar foreach en caso de mas-->
+					@endforeach
+				@else
+					<td>hola</td>
+				@endif
+
+			    <td>
+	              <a href="{{ action('CursoController@edit', $curso->nombre) }}" class="btn btn-warning">Editar</a>
+	            </td>
+
+				@component('layouts.boton_borrar')
+	              @slot('controlador_borrar')
+	                {{ Form::open(['action' => ['CursoController@destroy', $curso->nombre]]) }}
+	              @endslot
+	            @endcomponent
+
+
+			</tr>
+		  @endforeach
+		@endslot
+	  @endcomponent
 @endsection
