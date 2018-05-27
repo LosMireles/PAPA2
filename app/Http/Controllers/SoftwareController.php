98265d962933a850defda4f0f0928e3b5045269e
Jhon Smith <jsmith@localhost.localdomain>
2018-05-26 20:26:21 -0700

    Cambia controlador software e inciso 9.2.2
    Algunas partes del software fueron comentadas para que funcione por
    ahora.

--- app/Http/Controllers/SoftwareController.php
+++ app/Http/Controllers/SoftwareController.php
@@ -13,7 +13,7 @@ use App\Asignatura;
 
 class SoftwareController extends Controller {
 
-    private $clases = ["Lenguaje", "Libreria", "Case", "Manejador BD", "Otro"];
+    private $clases = ["Lenguaje", "Case", "Manejador BD", "Paqueteria", "Otro"];
 
     public function index(){
         $softwares  = Software::all();
@@ -23,11 +23,12 @@ class SoftwareController extends Controller {
 
     //----------------------------------------------------------------
     public function create(){
-        $equipos = Equipo::all();
-        $asignaturas = Asignatura::all();
+        //$equipos = Equipo::all();
+        //$asignaturas = Asignatura::all();
         return view('equipamiento.softwares.create')
-            ->with(['equipos'     => $equipos,
-                    'asignaturas' => $asignaturas,
+            ->with([
+                    //'equipos'     => $equipos,
+                    //'asignaturas' => $asignaturas,
                     'clases'      => $this->clases]);
 	}
 
@@ -39,28 +40,27 @@ class SoftwareController extends Controller {
         $software                 = new Software;
 
         $software->nombre         = $request->nombre;
-        $software->manualUsuario  = $request->manualUsuario;
-		$software->licencia       = $request->licencia;
-		$software->disponibilidad = $request->disponibilidad;
+		$software->version        = $request->version;
 		$software->clase          = $request->clase;
+		$software->disponibilidad = $request->disponibilidad;
         $equipos                  = $request->equipo;
         $asignaturas              = $request->asignatura;
 
         $software->save();
 
-        if(!empty($equipos)){
-            foreach($equipos as $serial){
-                $equipoId = DB::table('equipos')->where('serial', $serial)->value('id');
-                $software->equipos()->attach($equipoId);
-            }
-        }
+        //if(!empty($equipos)){
+            //foreach($equipos as $serial){
+                //$equipoId = DB::table('equipos')->where('serial', $serial)->value('id');
+                //$software->equipos()->attach($equipoId);
+            //}
+        //}
 
-        if(!empty($asignaturas)){
-            foreach($asignaturas as $nombre){
-                $asignaturaId = DB::table('asignaturas')->where('nombre', $nombre)->value('id');
-                $software->asignaturas()->attach($asignaturaId);
-            }
-        }
+        //if(!empty($asignaturas)){
+            //foreach($asignaturas as $nombre){
+                //$asignaturaId = DB::table('asignaturas')->where('nombre', $nombre)->value('id');
+                //$software->asignaturas()->attach($asignaturaId);
+            //}
+        //}
 
 		echo "Elemento insertado exitosamente!";
         return redirect()->action('SoftwareController@index');
@@ -68,8 +68,8 @@ class SoftwareController extends Controller {
 
 	//*-----------------------------------------------------------------
 	public function edit($nombre) {
-        $equipo_softwares      = DB::table('equipo_software')->get();
-        $asignaturas_softwares = DB::table('asignatura_software')->get();
+        //$equipo_softwares      = DB::table('equipo_software')->get();
+        //$asignaturas_softwares = DB::table('asignatura_software')->get();
 		$software              = Software::where('nombre', $nombre)->first();
 	  	$equipos               = Equipo::all();
 	  	$asignaturas           = asignatura::all();
@@ -79,49 +79,18 @@ class SoftwareController extends Controller {
                     'equipos'              => $equipos,
                     'asignaturas'          => $asignaturas,
                     'clases'               => $this->clases,
-                    'equipo_softwares'     => $equipo_softwares,
-                    'asignaturas_softwares' => $asignaturas_softwares]);
+                    //'equipo_softwares'     => $equipo_softwares,
+                    //'asignaturas_softwares' => $asignaturas_softwares
+                ]);
 	}
 
 	//*-----------------------------------------------------------------
     public function update(Request $request, $nombre){
         $request->validate($this->rules($nombre));
 
-	  	$nombre_nuevo      = $request->nombre;
-	  	$manualUsuario     = $request->manualUsuario;
-	  	$licencia          = $request->licencia;
-	  	$disponibilidad    = $request->disponibilidad;
-	  	$clase             = $request->clase;
-	  	$serialEquipos     = $request->equipos;
-	  	$nombreAsignaturas = $request->asignaturas;
-
         $software = Software::where('nombre', $nombre)->first();
 
-		$software->update([
-            'nombre'         => $nombre_nuevo,
-            'manualUsuario'  => $manualUsuario,
-            'licencia'       => $licencia,
-            'disponibilidad' => $disponibilidad,
-            'clase'          => $clase
-        ]);
-
-        if(!empty($serialEquipos)){
-            $idEquipos = [];
-            foreach($serialEquipos as $serial){
-                $idEquipos[] = Equipo::where('serial', $serial)->value('id');
-            }
-
-            $software->equipos()->sync($idEquipos);
-        }
-
-        if(!empty($nombreAsignaturas)){
-            $idAsignaturas = [];
-            foreach($nombreAsignaturas as $nombreAsignatura){
-                $idAsignaturas[] = asignatura::where('nombre', $nombreAsignatura)->value('id');
-            }
-
-            $software->asignaturas()->sync($idAsignaturas);
-        }
+        $software->update($request->all());
 
 		echo "Elemento editado exitosamente!";
         return redirect()->action('SoftwareController@index');
@@ -146,9 +115,6 @@ class SoftwareController extends Controller {
         return [
                 'nombre'         => ['required',
                                      Rule::unique('softwares')->ignore($nombre, 'nombre')],
-                'manualUsuario'  => 'required|boolean',
-                'licencia'       => 'required|alpha_num',
-                'disponibilidad' => 'required|alpha_num'
                ];
     }
 }
