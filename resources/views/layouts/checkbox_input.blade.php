0d3ece7b639c2b7fcfcbaf26f51d769deda10888
Jhon Smith <jsmith@localhost.localdomain>
2018-05-27 20:09:29 -0700

    Arregla incisos 9.1.6 y cambia los botones regresar

--- resources/views/layouts/checkbox_input.blade.php
+++ resources/views/layouts/checkbox_input.blade.php
@@ -5,6 +5,9 @@
     </label>
 
     <div class="col-sm-8">
+        <input type="hidden" name="{{$nombre_input}}" value="0" @yield('extra_checkbox')>
+        </input>
+
         <input type="checkbox" name="{{$nombre_input}}" value="1" @yield('extra_checkbox')>
             {{$descripcion or $label_input}}
         </input>
