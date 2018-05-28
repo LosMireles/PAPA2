172ad9fd2e34faa781012efc6128a9144127765c
jjups96 <jjups96@gmail.com>
2018-05-28 15:57:17 -0700

    Semaforo links

--- resources/views/layouts/componentes_inicio/formulario_salida.blade.php
+++ resources/views/layouts/componentes_inicio/formulario_salida.blade.php
@@ -10,18 +10,75 @@
 
 	@slot("cuerpo_tabla")
 		<tr>
-			<td>9.1</td>
+			<td>9.1 Inmobilario</td>
 			<td>
 				<ul class="list-unstyled">
-					<li>9.1.4</li>
-					<li>9.1.6</li>
-					<li>9.1.7</li>
-					<li>9.1.8</li>
-					<li>9.1.9</li>
-					<li>9.1.10</li>
-					<li>9.1.11</li>
-					<li>9.1.12</li>
-					<li>9.1.13</li>
+					<li>
+						@component("layouts.componentes.tooltip_general_link")
+							@slot('url')
+								{{ url('/inciso_9_1_4') }}
+							@endslot
+							@slot('titulo', 'Haga clic para ir al inciso')
+							@slot('mensaje', '9.1.4 (Perfil y experiencia de los responsables de los servicios de cómputo)')
+						@endcomponent					
+					</li>
+					<li>@component("layouts.componentes.tooltip_general_link")
+							@slot('url')
+								{{ url('/inciso_9_1_6') }}
+							@endslot
+							@slot('titulo', 'Haga clic para ir al inciso')
+							@slot('mensaje', '9.1.6 (Características de las aulas)')
+						@endcomponent</li>
+					<li>@component("layouts.componentes.tooltip_general_link")
+							@slot('url')
+								{{ url('/inciso_9_1_7') }}
+							@endslot
+							@slot('titulo', 'Haga clic para ir al inciso')
+							@slot('mensaje', '9.1.7 (Mostrar la relación aulas - cursos)')
+						@endcomponent</li>
+					<li>@component("layouts.componentes.tooltip_general_link")
+							@slot('url')
+								{{ url('/inciso_9_1_8') }}
+							@endslot
+							@slot('titulo', 'Haga clic para ir al inciso')
+							@slot('mensaje', '9.1.8 (Equipo de cómputo y audiovisual en las aulas)')
+						@endcomponent</li>
+					<li>@component("layouts.componentes.tooltip_general_link")
+							@slot('url')
+								{{ url('/inciso_9_1_9') }}
+							@endslot
+							@slot('titulo', 'Haga clic para ir al inciso')
+							@slot('mensaje', '9.1.9 (Cubículos y áreas de profesores)')
+						@endcomponent
+					</li>
+					<li>@component("layouts.componentes.tooltip_general_link")
+							@slot('url')
+								{{ url('/inciso_9_1_10') }}
+							@endslot
+							@slot('titulo', 'Haga clic para ir al inciso')
+							@slot('mensaje', '9.1.10 (Espacios para asesoría y características)')
+						@endcomponent</li>
+					<li>@component("layouts.componentes.tooltip_general_link")
+							@slot('url')
+								{{ url('/inciso_9_1_11') }}
+							@endslot
+							@slot('titulo', 'Haga clic para ir al inciso')
+							@slot('mensaje', '9.1.11 (Auditorios y actividades que ocurren en ellos)')
+						@endcomponent</li>
+					<li>@component("layouts.componentes.tooltip_general_link")
+							@slot('url')
+								{{ url('/inciso_9_1_12') }}
+							@endslot
+							@slot('titulo', 'Haga clic para ir al inciso')
+							@slot('mensaje', '9.1.12 (Auditorios y su higiene)')
+						@endcomponent</li>
+					<li>@component("layouts.componentes.tooltip_general_link")
+							@slot('url')
+								{{ url('/inciso_9_1_13') }}
+							@endslot
+							@slot('titulo', 'Haga clic para ir al inciso')
+							@slot('mensaje', '9.1.13 (Características de los Sanitarios)')
+						@endcomponent</li>
 				</ul>
 			</td>
 
@@ -351,16 +408,58 @@
 		</tr>
 
 		<tr>
-			<td>9.2</td>
+			<td>9.2 Equipo</td>
 			<td>
 				<ul class="list-unstyled">
-					<li>9.2.1</li>
-					<li>9.2.2</li>
-					<li>9.2.5</li>
-					<li>9.2.7</li>
-					<li>9.2.11</li>
-					<li>9.2.12</li>
-					<li>9.2.14</li>
+					<li>@component("layouts.componentes.tooltip_general_link")
+							@slot('url')
+								{{ url('/inciso_9_2_1') }}
+							@endslot
+							@slot('titulo', 'Haga clic para ir al inciso')
+							@slot('mensaje', '9.2.1 (Software usados por asignatura)')
+						@endcomponent</li>
+					<li>@component("layouts.componentes.tooltip_general_link")
+							@slot('url')
+								{{ url('/inciso_9_2_2') }}
+							@endslot
+							@slot('titulo', 'Haga clic para ir al inciso')
+							@slot('mensaje', '9.2.2 (Tipos de Software del programa (case, lenguajes, manejadores de BD, paquetería en general))')
+						@endcomponent</li>
+					<li>@component("layouts.componentes.tooltip_general_link")
+							@slot('url')
+								{{ url('/inciso_9_2_5') }}
+							@endslot
+							@slot('titulo', 'Haga clic para ir al inciso')
+							@slot('mensaje', '9.2.5 (Plataformas de cómputo (Sistemas operativos))')
+						@endcomponent</li>
+					<li>@component("layouts.componentes.tooltip_general_link")
+							@slot('url')
+								{{ url('/inciso_9_2_7') }}
+							@endslot
+							@slot('titulo', 'Haga clic para ir al inciso')
+							@slot('mensaje', '9.2.7 (Redes disponibles para el programa)')
+						@endcomponent</li>
+					<li>@component("layouts.componentes.tooltip_general_link")
+							@slot('url')
+								{{ url('/inciso_9_2_11') }}
+							@endslot
+							@slot('titulo', 'Haga clic para ir al inciso')
+							@slot('mensaje', '9.2.11 (Equipo de cómputo de maestros)')
+						@endcomponent</li>
+					<li>@component("layouts.componentes.tooltip_general_link")
+							@slot('url')
+								{{ url('/inciso_9_2_12') }}
+							@endslot
+							@slot('titulo', 'Haga clic para ir al inciso')
+							@slot('mensaje', '9.2.12 (Soporte técnico)')
+						@endcomponent</li>
+					<li>@component("layouts.componentes.tooltip_general_link")
+							@slot('url')
+								{{ url('/inciso_9_2_14') }}
+							@endslot
+							@slot('titulo', 'Haga clic para ir al inciso')
+							@slot('mensaje', '9.2.14 (Currículum del personal técnico)')
+						@endcomponent</li>
 				</ul>
 			</td>
 
