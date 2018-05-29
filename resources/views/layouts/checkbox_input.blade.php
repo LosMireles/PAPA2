593b1b26324b29784fa9be6de6ce93b538d7f4af
Jhon Smith <jsmith@localhost.localdomain>
2018-05-28 21:14:34 -0700

    Arregla vista create de aulas
    Pero quiza rompa otras vistas. Falta probarlo.

--- resources/views/layouts/checkbox_input.blade.php
+++ resources/views/layouts/checkbox_input.blade.php
@@ -1,15 +1,14 @@
 <div class="form-group">
 
-    <label for="{{$nombre_input}}" class="col-sm-4 control-label" data-toggle="tooltip" title="{{$tooltip_input}}">
+    <label for="{{$nombre_input}}" class="col-sm-8 control-label" data-toggle="tooltip" title="{{$tooltip_input}}">
         {{$label_input}}
     </label>
 
-    <div class="col-sm-8">
+    <div class="col-sm-4">
         <input type="hidden" name="{{$nombre_input}}" value="0" @yield('extra_checkbox')>
         </input>
 
         <input type="checkbox" name="{{$nombre_input}}" value="1" @yield('extra_checkbox')>
-            {{$descripcion or $label_input}}
         </input>
     </div>
 
--- resources/views/layouts/checkbox_input2.blade.php
+++ resources/views/layouts/checkbox_input2.blade.php
@@ -1,21 +1,19 @@
 <div class="form-group">
 
-    <label for="{{$nombre_input}}" class="col-sm-4 control-label" data-toggle="tooltip" title="{{$tooltip_input}}">
+    <label for="{{$nombre_input}}" class="col-sm-8 control-label" data-toggle="tooltip" title="{{$tooltip_input}}">
         {{$label_input}}
     </label>
 
-    <div class="col-sm-8">
+    <div class="col-sm-4">
         @if($variable)
             <input type="hidden" name="{{$nombre_input}}" value="0" checked @yield('extra_checkbox')>
             </input>
             <input type="checkbox" name="{{$nombre_input}}" value="1" checked @yield('extra_checkbox')>
-                {{$descripcion or $label_input}}
             </input>
         @else
             <input type="hidden" name="{{$nombre_input}}" value="0" @yield('extra_checkbox')>
             </input>
             <input type="checkbox" name="{{$nombre_input}}" value="1" @yield('extra_checkbox')>
-                {{$descripcion or $label_input}}
             </input>
         @endif
     </div>
